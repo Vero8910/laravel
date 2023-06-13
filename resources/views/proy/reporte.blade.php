fe9444d3d8f457f28622f50f2139d01e177bd55b
Veronica Maria Marroquin Orellana <veronica.marroquin@upisss.gob.sv>
2023-06-13 15:10:32 -0600

    comienzo del proyecto

---
+++ resources/views/proy/reporte.blade.php
@@ -0,0 +1,37 @@
+<center><strong>Gobierno de El Salvador </strong></center>
+<center><strong>Instituto SalvadoreÃ±o del Seguro Social<strong></center>
+<br>
+<center><strong>Listado de Proyectos<strong></center>
+Fecha: <?php
+     $mytime = Carbon\Carbon::now();
+     echo $mytime->toDateTimeString();
+    ?>
+<br>
+
+<table class="table table-stripped table-hover">
+
+    <tr>
+        <th>#</th>
+        <th>Nombre del Proyecto</th>
+        <th>Fuente de Fondos</th>
+        <th>Monto Planificado</th>
+        <th>Monto Patrocinado</th>
+        <th>Monto Fondos Propios</th>
+   </tr>
+    <tbody>
+        @foreach($Proyecto as $proyecto)
+        <tr>
+            <td>{{$proyecto->id}}</td>
+            <td>{{$proyecto->NombreProyecto}} </td>
+            <td>{{$proyecto->FuenteFondos}}</td>
+            <td align="right">
+
+                {{$proyecto->MontoPlanificado}}
+
+            </td>
+            <td align="right">{{$proyecto->MontoPatrocinado}}</td>
+            <td align="right">{{$proyecto->MontoFondosPropios}}</td>
+        @endforeach
+
+    </tbody>
+</table>
